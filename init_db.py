077bd780a03a9b77dee5c9936f89ea8b1462aeb2
Goran Obradovic <goran.obradovic@gmail.com>
2025-03-25 17:43:59 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -7,9 +7,6 @@ import sys
 # Add the current directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-# Force SQLite database
-os.environ['DATABASE_URI'] = 'sqlite:///brevity.db'
-
 from app import create_app, db
 from models import User, Post, Settings
 
