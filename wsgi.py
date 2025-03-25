268a07523093c5031a1bbb99457d3dba95579022
Goran Obradovic <goran.obradovic@gmail.com>
2025-03-25 19:47:03 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 from app import app
 
+from datetime import datetime
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()} 
+
 if __name__ == "__main__":
     app.run() 
